1cf551d14916bb7da732971d3fc29f17b3371511
Rebekah Rowe <rebekahrowe9999@gmail.com>
2022-04-04 14:46:31 -0400

    Added a couple of doxygen comments to gui.h

--- include/libpdraw/gui/gui.hpp
+++ include/libpdraw/gui/gui.hpp
@@ -14,13 +14,19 @@ class IWidget;
 #include "canvas.hpp"
 #include "input.hpp"
 
+/// Main root object of libpdraw
+/**
+ * The main required global of the library.
+ * You MUST set g_pGUI to a valid pointer and run Setup() afterwards for anything to function.
+ */
 class CatGUI {
 public:
     CatGUI();
     ~CatGUI();
 
     void Setup();
-    Canvas* GetRootWindow();
+    Canvas* GetRootWindow(); /**< Use to get a container you can add widgets to. */
     Canvas* m_pRootWindow;
 };
+/** Must be a valid pointer for library to function. */
 extern CatGUI* g_pGUI;
